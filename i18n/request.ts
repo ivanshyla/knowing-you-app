21359b4da4a2560f1ed35de470a905f4b11eacac
Ivan <salihorsk@gmail.com>
2026-01-01 23:09:47 +0100

    fix: static import for i18n messages

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,23 @@
 import { getRequestConfig } from 'next-intl/server'
 import { cookies } from 'next/headers'
 
+import enMessages from '../messages/en.json'
+import ruMessages from '../messages/ru.json'
+import plMessages from '../messages/pl.json'
+import ukMessages from '../messages/uk.json'
+import beMessages from '../messages/be.json'
+
 export const locales = ['en', 'ru', 'pl', 'uk', 'be'] as const
 export type Locale = (typeof locales)[number]
 export const defaultLocale: Locale = 'en'
+
+const messages: Record<Locale, typeof enMessages> = {
+  en: enMessages,
+  ru: ruMessages,
+  pl: plMessages,
+  uk: ukMessages,
+  be: beMessages
+}
 
 export default getRequestConfig(async () => {
   const cookieStore = await cookies()
@@ -11,6 +25,6 @@ export default getRequestConfig(async () => {
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: messages[locale]
   }
 })
