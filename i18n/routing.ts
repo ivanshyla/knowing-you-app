d7055d4a826504e1ccc46f7dc6b423322515a75c
Ivan <salihorsk@gmail.com>
2026-01-06 01:33:18 +0100

    fix(i18n): use localePrefix mode object

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,7 @@ import { createNavigation } from 'next-intl/navigation'
 export const routing = defineRouting({
   locales: ['en', 'ru', 'pl', 'uk', 'be'],
   defaultLocale: 'en',
-  localePrefix: 'always' // Always show prefix in URL (e.g. /en, /ru)
+  localePrefix: { mode: 'always' } // Always show prefix in URL (e.g. /en, /ru)
 })
 
 // Lightweight wrappers around Next.js navigation APIs
